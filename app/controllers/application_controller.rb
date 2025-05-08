c878f1c0764f50194f7860dbc90e74775f28631c
MaoKaneda <maddieqqie@gmail.com>
2025-05-08 16:31:38 +0900

    ログイン画面遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,24 @@
-before_action :configure_permitted_parameters, if: :devise_controller?
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-protected
+  protected
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
-  devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :occupation, :position])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :occupation, :position])
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path
+  end
 end
 
 rails generate devise:views
-
-def after_sign_in_path_for(resource)
-  root_path
-end
-
-def after_sign_out_path_for(resource_or_scope)
-  root_path
-end
-
-def after_sign_up_path_for(resource)
-  root_path
-end
