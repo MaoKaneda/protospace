f2f33ec5ee82b2f40a33cbc411e191608444cb3d
MaoKaneda <maddieqqie@gmail.com>
2025-05-11 01:35:42 +0900

    コントローラー機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,55 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
   def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = current_user.prototypes.build(prototype_params)
+    if @prototype.save
+      redirect_to root_path, notice: 'プロトタイプを投稿しました'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to root_path, notice: 'プロトタイプを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path, notice: 'プロトタイプを削除しました'
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless @prototype.user == current_user
   end
 end
