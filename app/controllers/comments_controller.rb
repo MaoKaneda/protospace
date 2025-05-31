957575e37eb3f3410387ac61ee4e461ca0debe1d
MaoKaneda <maddieqqie@gmail.com>
2025-06-01 00:45:48 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,20 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
 
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to prototype_path(@comment.prototype)
+      redirect_to prototype_path(params[:prototype_id])
     else
-      @prototype = @comment.prototype
-      @comments = @prototype.comments
+      @prototype = Prototype.find(params[:id])
+      @comment = Comment.new
+      @comments = @prototype.comments.includes(:user)
       render "prototypes/show", status: :unprocessable_entity
     end
-  end
 
-  def move_to_index
-    unless user_signed_in? && current_user == @prototype.user
-      redirect_to action: :index
-    end
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
