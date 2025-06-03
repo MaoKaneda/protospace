fae541da5069b13a8ef6d385ff1319e079799e47
MaoKaneda <maddieqqie@gmail.com>
2025-06-03 15:25:26 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,7 @@
 # exit on error
 set -o errexit
 
-# 依存関係のインストール
 bundle install
-
-# アセットのプリコンパイル
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-
-# データベースのマイグレーション
-bundle exec rake db:migrate 
+bundle exec rake db:migrate
