72eec9c46592c78293fd09bd1835e258f414c143
MaoKaneda <maddieqqie@gmail.com>
2025-06-03 15:22:37 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true
-  validates :profile, presence: true
-  validates :occupation, presence: true
-  validates :position, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates :password, presence: true, length: { minimum: 6 }, on: :create
-  validates :password_confirmation, presence: true, on: :create
+  validates :name,          presence: true
+  validates :profile,       presence: true
+  validates :occupation,    presence: true
+  validates :position,      presence: true
 
   has_many :prototypes
   has_many :comments
